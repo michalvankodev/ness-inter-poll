ba3e83227b860e57e821af83b66a4e0ef56b8570
Tomas Lojka <lojka_tomas@solarturbines.com>
2018-05-05 14:52:34 +0200

    Added Close button

---
+++ nesspoll/src/components/CloseButton.tsx
@@ -0,0 +1,32 @@
+import * as React from 'react';
+import { Redirect } from 'react-router-dom';
+import IconClose from 'material-ui/svg-icons/navigation/close';
+
+class CloseButton extends React.Component<any, any> {
+    constructor(props: any) {
+        super(props);
+        this.state = {
+          redirect: false,
+        };
+    }
+
+    handleOnClickClose() {
+        this.setState({redirect: true});
+    }
+
+    render() {
+        const { redirect } = this.state;
+    
+        if (redirect) {
+          return <Redirect to="/"/>;
+        }
+
+        return (
+          <div>
+            <IconClose title="Close" class="close_button" onClick={() => this.handleOnClickClose()}/>            
+          </div>
+        );
+    }
+}
+
+export default CloseButton;
